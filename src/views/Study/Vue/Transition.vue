12c6e153b74954e5d4c1e30d90a4f3b1ab21b090
615940742@qq.com <whtlikegxy2004>
2020-07-04 21:38:46 +0800

    【vue】【transition】学习

--- src/views/Study/Vue/Transition.vue
+++ src/views/Study/Vue/Transition.vue
@@ -6,34 +6,38 @@
         <div class="panel-body panel-body-box">
             <a-button type="primary" @click="handleClick">点击</a-button>
 
-            <div class="flex">
+            <div>
                 <!-- 无过渡效果 -->
                 <div v-if="isShow" class="box">无过渡效果</div>
                 <!-- 有过渡效果 -->
-                <transition name="fade"
-                            appear
-                            :duration="1000"
-                            enter-active-class="animated fadeInDown"
-                            leave-active-class="animated bounceOutRight"
-                            @before-enter="beforeEnter"
-                            @enter="enter"
-                            @after-enter="afterEnter"
-                            @enter-cancelled="enterCancelled"
-                            @before-leave="beforeLeave"
-                            @leave="leave"
-                            @after-leave="afterLeave"
-                            @leave-cancelled="leaveCancelled"
-                >
-                    <div v-if="isShow" class="box">有过渡效果1</div>
+<!--                <transition name="fade"-->
+<!--                            appear-->
+<!--                            :duration="1000"-->
+<!--                            enter-active-class="animated fadeInDown"-->
+<!--                            leave-active-class="animated bounceOutRight"-->
+<!--                            @before-enter="beforeEnter"-->
+<!--                            @enter="enter"-->
+<!--                            @after-enter="afterEnter"-->
+<!--                            @enter-cancelled="enterCancelled"-->
+<!--                            @before-leave="beforeLeave"-->
+<!--                            @leave="leave"-->
+<!--                            @after-leave="afterLeave"-->
+<!--                            @leave-cancelled="leaveCancelled"-->
+<!--                >-->
+<!--                    <div v-if="isShow" class="box">有过渡效果1</div>-->
+<!--                </transition>-->
+
+                <transition name="fade" :duration="3000">
+                    <div class="box" v-if="isShow">
+                        <p>hello</p>
+                    </div>
                 </transition>
 
-                <!--<transition name="fadeIn" duration="3000">
-                    <div v-if="isShow" class="box">有过渡效果2</div>
+                <transition name="slide-fade" :duration="1000">
+                    <div class="box" v-if="isShow">
+                        <p>hello</p>
+                    </div>
                 </transition>
-
-                <transition name="fadeIn" :duration="{ enter: 1000, leave: 2000 }">
-                    <div v-if="isShow" class="box">有过渡效果3</div>
-                </transition>-->
             </div>
         </div>
     </div>
@@ -99,11 +103,32 @@
 </script>
 
 <style lang="less" scoped>
-    .box {
-        width: 200px;
-        height: 200px;
-        background-color: #FF456E;
-        margin-right: 10px;
+
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to { /* .fade-leave-active below version 2.1.8 */
+        opacity: 0;
     }
 
+    /* 可以设置不同的进入和离开动画 */
+    /* 设置持续时间和动画函数 */
+    .slide-fade-enter-active {
+        transition: all .3s ease;
+    }
+    .slide-fade-leave-active {
+        transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to
+        /* .slide-fade-leave-active for below version 2.1.8 */ {
+        transform: translateX(10px);
+        opacity: 0;
+    }
+
+
+    .box {
+        height: 200px;
+        background-color: #FF456E;
+        margin-bottom: 30px;
+    }
 </style>
